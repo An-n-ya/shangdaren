5f9bb4d6f145817e55e1625cbc3f035925dfd69b
Annya <annya.host@gmail.com>
2024-04-25 18:00:19 +0800

    sort hand card and relayout hand card

--- main.js
+++ main.js
@@ -1,22 +1,9 @@
 import './style.css'
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
+// 规则
+/*
+
+*/
 
 function Card(type, selected, id) {
     this.type = type;
@@ -36,9 +23,9 @@ let cards_arr = [];
 let hand = [];
 let total = [];
 
-for (let i = 0; i < 4; i++) {
-    for (let j = 0; j < card_face.length; j++) {
-        let id = i * card_face.length + j;
+for (let j = 0; j < card_face.length; j++) {
+    for (let i = 0; i < 4; i++) {
+        let id = j * 4 + i;
         cards_arr.push(new Card(card_face[j], false, id));
     }
 }
@@ -52,7 +39,7 @@ function getRandomInt(max) {
 
 function start() {
     let total = cards_arr.map(a => ({...a}));;
-    for (let i = 0; i < 12; i++) {
+    for (let i = 0; i < 19; i++) {
         let ind = getRandomInt(total.length);
         let card = total[ind];
         total.splice(ind, 1);
@@ -60,17 +47,46 @@ function start() {
     }
 }
 
+function sort_hand() {
+    hand.sort((a, b) =>
+        a.id - b.id
+    )
+}
+
 
 function render() {
     let container = document.querySelector('#cards');
     container.innerHTML = "";
+    sort_hand();
+    let cur_group = 1;
+    let group = document.createElement("div");
+            group.id = "group";
     for (let i = 0; i < hand.length; i++) {
         let card = hand[i];
-        container.appendChild(create_card(card));
+        if (card.id >= (cur_group - 1) * 12 && card.id < cur_group * 12) {
+            group.appendChild(create_card(card));
+        } else {
+            while (cur_group * 12 <= card.id) {
+                cur_group += 1;
+            }
+            if (group.childNodes.length != 0) {
+                group.style.maxHeight = "" + ((group.childNodes.length)*50) + "px";
+                container.appendChild(group);
+            }
+            group = document.createElement("div");
+            group.id = "group";
+            group.appendChild(create_card(card));
+        }
+    }
+    if (group.childNodes.length != 0) {
+        group.style.maxHeight = "" + ((group.childNodes.length)*50) + "px";
+        container.appendChild(group);
     }
 }
 
 function create_card(card) {
+    let div = document.createElement("div");
+    div.id = "card-wrapper";
     let img = document.createElement("img");
     img.src = "上大人/" + card.type + ".png";
     img.addEventListener('click', () => {
@@ -86,7 +102,9 @@ function create_card(card) {
     img.id = "card";
     if (card.selected) {
         img.classList.add("selected");
+        div.classList.add("selected");
     }
+    div.appendChild(img);
 
-    return img;
+    return div;
 }
